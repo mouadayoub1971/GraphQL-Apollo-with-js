01283f3869e2f88516555bf4c4e65baf783ca557
mouad ayoub <mouadayoubmouadayoub@gmail.com>
2025-01-29 13:05:14 +0100

    implement schema

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema";
 
 // server setup 
 const server = new ApolloServer({
- 
+ typeDefs
 })
+
+const { url } = await startStandaloneServer(server, {
+ listen: { port: 4000}
+})
+
+console.log('Server ready at port ', 4000)
