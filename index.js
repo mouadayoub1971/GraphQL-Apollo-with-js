ec7ed4bc9c3187ffbf70b936b25cd26de79fd366
mouad ayoub <mouadayoubmouadayoub@gmail.com>
2025-01-30 23:42:09 +0100

    related data

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { typeDefs } from "./schema";
+import { typeDefs } from "./schema.js";
+import db from "./_db.js"
 
-// server setup 
+const resolvers = {
+ Query: {
+  games() {
+   return db.games
+  },
+  authors() {
+   return db.authors
+  },
+  reviews() {
+   return db.reviews
+  },
+  review(_, args, context) {
+   return db.reviews.find((item) => item.id === args.id)
+  },
+  game(_, args){
+      return db.games.find((game)=> game.id === args.id)
+  },
+  author(_, args){
+      return db.authors.find((author)=> author.id === args.id)
+  }
+ },
+ Game: {
+  reviews(parent) {
+   return db.reviews.filter((r) => r.game_id === parent.id)
+  }
+ }
+}
+// server setup
 const server = new ApolloServer({
- typeDefs
+ typeDefs,
+ resolvers
 })
 
 const { url } = await startStandaloneServer(server, {
--- schema.js
+++ schema.js
@@ -3,20 +3,27 @@ export const typeDefs = `#graphql
   id: ID!
   title: String!
   platform: [String!]!
+  reviews: [Review!]
  }
  type Review {
   id: ID!
   rating: Int!
   content: String!
+  game: Game!
+  author: Author!
  }
  type Author {
   id: ID!
   name: String!
   verified: Boolean!
+  reviews: [Review!]
  }
  type Query {
-  reviews: [Reviews]
+  reviews: [Review]
   games: [Game]
   authors: [Author]
+  review(id: ID!): Review
+  author(id: ID!): Author
+  game(id: ID!): Game
  }
 `
